33c735cb756b1a76b7e470c77bef688be8117279
FabiOyuela <48002144+FabiOyuela@users.noreply.github.com>
2019-02-26 21:35:10 -0600

    Create servicios.java

---
+++ src/interfaz/servicios.java
@@ -0,0 +1,54 @@
+package interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+
+public class servicios extends JFrame {
+
+	private JPanel contentPane;
+	private final JTextPane textPane = new JTextPane();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					servicios frame = new servicios();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public servicios() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblServicios = new JLabel("SERVICIOS");
+		lblServicios.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblServicios, BorderLayout.NORTH);
+		contentPane.add(textPane, BorderLayout.CENTER);
+		
+		JTextPane textPane_1 = new JTextPane();
+		contentPane.add(textPane_1, BorderLayout.WEST);
+	}
+
+}
