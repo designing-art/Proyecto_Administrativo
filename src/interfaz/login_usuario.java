e72060744e18d77d658f5ebc21f41b71751ef868
KrizPLAY <krizemandiaz11@gmail.com>
2019-03-12 08:00:18 -0600

    Update login_usuario.java

--- src/interfaz/login_usuario.java
+++ src/interfaz/login_usuario.java
@@ -8,10 +8,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Window.Type;
+import javax.swing.JLabel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class login_usuario extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -40,8 +48,56 @@ public class login_usuario extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(140, 157, 435, 180);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario :");
+		lblUsuario.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 15));
+		lblUsuario.setBounds(61, 64, 113, 14);
+		panel.add(lblUsuario);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a :");
+		lblContrasea.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 15));
+		lblContrasea.setBounds(61, 95, 113, 14);
+		panel.add(lblContrasea);
+		
+		JLabel lblNewLabel = new JLabel("Olvido su contrase\u00F1a?");
+		lblNewLabel.setBounds(164, 126, 162, 14);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(164, 64, 220, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(164, 95, 220, 20);
+		panel.add(textField_1);
+		
+		JLabel lblLogin = new JLabel("Ingresar");
+		lblLogin.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblLogin.setBounds(10, 11, 162, 32);
+		panel.add(lblLogin);
+		
+		JButton btnNewButton = new JButton("Logo");
+		btnNewButton.setBounds(267, 11, 169, 137);
+		contentPane.add(btnNewButton);
+		
+		JButton btnEntrar = new JButton("ENTRAR");
+		btnEntrar.setBounds(140, 348, 141, 34);
+		contentPane.add(btnEntrar);
+		
+		JButton btnRegistrar = new JButton("REGISTRAR");
+		btnRegistrar.setBounds(419, 348, 156, 34);
+		contentPane.add(btnRegistrar);
 	}
 
 }
