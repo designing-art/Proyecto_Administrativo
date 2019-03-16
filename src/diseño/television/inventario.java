09e6af651e05dd04176eddccfe569a43cff4b9fa
CarmenCeci <48001894+CarmenCeci@users.noreply.github.com>
2019-03-15 23:54:29 -0600

    update

--- src/diseño/television/inventario.java
+++ src/diseño/television/inventario.java
@@ -1,3 +1,4 @@
+
 package diseño.television;
 
 import java.awt.BorderLayout;
@@ -15,7 +16,7 @@ import javax.swing.JTextField;
 import javax.swing.JTable;
 import javax.swing.border.MatteBorder;
 
-public class empleados extends JFrame {
+public class inventario extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
@@ -28,7 +29,7 @@ public class empleados extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					empleados frame = new empleados();
+					inventario frame = new inventario();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -40,7 +41,7 @@ public class empleados extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public empleados() {
+	public inventario() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 500);
 		contentPane = new JPanel();
@@ -49,14 +50,14 @@ public class empleados extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblEmpleadosRegistrados = new JLabel("EMPLEADOS");
-		lblEmpleadosRegistrados.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblEmpleadosRegistrados.setBounds(297, 11, 72, 28);
-		contentPane.add(lblEmpleadosRegistrados);
+		JLabel lblInventarioRegistrado = new JLabel("INVENTARIO");
+		lblInventarioRegistrado.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblInventarioRegistrado.setBounds(297, 11, 72, 28);
+		contentPane.add(lblInventarioRegistrado);
 		
-		JLabel lblBuscarEmpleado = new JLabel("Busqueda de empleados :");
-		lblBuscarEmpleado.setBounds(77, 53, 125, 14);
-		contentPane.add(lblBuscarEmpleado);
+		JLabel lblBuscarInventario = new JLabel("Busqueda de inventario :");
+		lblBuscarInventario.setBounds(77, 53, 125, 14);
+		contentPane.add(lblBuscarInventario);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(213, 50, 156, 20);
@@ -72,10 +73,10 @@ public class empleados extends JFrame {
 		table.setBounds(30, 78, 617, 322);
 		contentPane.add(table);
 		
-		JButton btnRegistrarEmpleado = new JButton("Registrar Empleado");
-		btnRegistrarEmpleado.setBackground(Color.GREEN);
-		btnRegistrarEmpleado.setBounds(30, 411, 154, 23);
-		contentPane.add(btnRegistrarEmpleado);
+		JButton btnRegistrarInventario = new JButton("Registrar Producto");
+		btnRegistrarInventario.setBackground(Color.GREEN);
+		btnRegistrarInventario.setBounds(30, 411, 154, 23);
+		contentPane.add(btnRegistrarInventario);
 		
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.setBackground(Color.RED);
--- src/diseño/television/proveedores.java
+++ src/diseño/television/proveedores.java
@@ -15,7 +15,7 @@ import javax.swing.JTextField;
 import javax.swing.JTable;
 import javax.swing.border.MatteBorder;
 
-public class empleados extends JFrame {
+public class proveedores extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
@@ -28,7 +28,7 @@ public class empleados extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					empleados frame = new empleados();
+					proveedores frame = new proveedores();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -40,7 +40,7 @@ public class empleados extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public empleados() {
+	public proveedores() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 500);
 		contentPane = new JPanel();
@@ -49,14 +49,14 @@ public class empleados extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblEmpleadosRegistrados = new JLabel("EMPLEADOS");
-		lblEmpleadosRegistrados.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblEmpleadosRegistrados.setBounds(297, 11, 72, 28);
-		contentPane.add(lblEmpleadosRegistrados);
+		JLabel lblRegistrarProveedores = new JLabel("PROVEEDORES");
+		lblRegistrarProveedores.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblRegistrarProveedores.setBounds(297, 11, 72, 28);
+		contentPane.add(lblRegistrarProveedores);
 		
-		JLabel lblBuscarEmpleado = new JLabel("Busqueda de empleados :");
-		lblBuscarEmpleado.setBounds(77, 53, 125, 14);
-		contentPane.add(lblBuscarEmpleado);
+		JLabel lblBuscarProveedor = new JLabel("Busqueda de proveedores :");
+		lblBuscarProveedor.setBounds(77, 53, 125, 14);
+		contentPane.add(lblBuscarProveedor);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(213, 50, 156, 20);
@@ -72,10 +72,10 @@ public class empleados extends JFrame {
 		table.setBounds(30, 78, 617, 322);
 		contentPane.add(table);
 		
-		JButton btnRegistrarEmpleado = new JButton("Registrar Empleado");
-		btnRegistrarEmpleado.setBackground(Color.GREEN);
-		btnRegistrarEmpleado.setBounds(30, 411, 154, 23);
-		contentPane.add(btnRegistrarEmpleado);
+		JButton btnRegistrarProveedor = new JButton("Registrar Proveedor");
+		btnRegistrarProveedor.setBackground(Color.GREEN);
+		btnRegistrarProveedor.setBounds(30, 411, 154, 23);
+		contentPane.add(btnRegistrarProveedor);
 		
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.setBackground(Color.RED);
