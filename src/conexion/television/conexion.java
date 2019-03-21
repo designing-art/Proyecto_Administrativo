4210978db102c03ba73d79dd2e0db38581569c6c
KrizPLAY <krizemandiaz11@gmail.com>
2019-03-20 19:16:52 -0600

    update

--- src/conexion/television/conexion.java
+++ src/conexion/television/conexion.java
@@ -10,7 +10,7 @@ public class conexion {
 	
 	private final String base = "television";
     private final String user = "root";
-    private final String password = "";
+    private final String password = "1234";
     private final String url = "jdbc:mysql://localhost:3306/" + base;
     private Connection con = null;
     
