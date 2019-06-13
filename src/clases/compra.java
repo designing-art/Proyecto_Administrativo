5578748848e4c2f8439b5b3a827c08c78eaf0466
KrizPLAY <krizemandiaz11@gmail.com>
2019-06-12 21:44:05 -0600

    crear compra

---
+++ src/clases/compra.java
@@ -0,0 +1,95 @@
+package clases;
+
+public class compra {
+	int id_compra;
+	String nombre_objeto_compra;
+	Double precio_objeto_compra;
+	String descripcion_objeto_compra;
+	String peso_objeto_compra;
+	String color_objeto_compra;
+	String marca_objeto_compra;
+	String modelo_objeto_compra;
+	int cantidad_objeto_compra;
+	String fecha_registro_compra;
+
+	public int getId_compra() {
+		return id_compra;
+	}
+
+	public void setId_compra(int id_compra) {
+		this.id_compra = id_compra;
+	}
+
+	public String getNombre_objeto_compra() {
+		return nombre_objeto_compra;
+	}
+
+	public void setNombre_objeto_compra(String nombre_objeto_compra) {
+		this.nombre_objeto_compra = nombre_objeto_compra;
+	}
+
+	public Double getPrecio_objeto_compra() {
+		return precio_objeto_compra;
+	}
+
+	public void setPrecio_objeto_compra(Double precio_objeto_compra) {
+		this.precio_objeto_compra = precio_objeto_compra;
+	}
+
+	public String getDescripcion_objeto_compra() {
+		return descripcion_objeto_compra;
+	}
+
+	public void setDescripcion_objeto_compra(String descripcion_objeto_compra) {
+		this.descripcion_objeto_compra = descripcion_objeto_compra;
+	}
+
+	public String getPeso_objeto_compra() {
+		return peso_objeto_compra;
+	}
+
+	public void setPeso_objeto_compra(String peso_objeto_compra) {
+		this.peso_objeto_compra = peso_objeto_compra;
+	}
+
+	public String getColor_objeto_compra() {
+		return color_objeto_compra;
+	}
+
+	public void setColor_objeto_compra(String color_objeto_compra) {
+		this.color_objeto_compra = color_objeto_compra;
+	}
+
+	public String getMarca_objeto_compra() {
+		return marca_objeto_compra;
+	}
+
+	public void setMarca_objeto_compra(String marca_objeto_compra) {
+		this.marca_objeto_compra = marca_objeto_compra;
+	}
+
+	public String getModelo_objeto_compra() {
+		return modelo_objeto_compra;
+	}
+
+	public void setModelo_objeto_compra(String modelo_objeto_compra) {
+		this.modelo_objeto_compra = modelo_objeto_compra;
+	}
+
+	public int getCantidad_objeto_compra() {
+		return cantidad_objeto_compra;
+	}
+
+	public void setCantidad_objeto_compra(int cantidad_objeto_compra) {
+		this.cantidad_objeto_compra = cantidad_objeto_compra;
+	}
+
+	public String getFecha_registro_compra() {
+		return fecha_registro_compra;
+	}
+
+	public void setFecha_registro_compra(String fecha_registro_compra) {
+		this.fecha_registro_compra = fecha_registro_compra;
+	}
+
+}
